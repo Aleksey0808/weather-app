f542c0d49156c6ccb3f0b8d5e2fc9a8fd576f29b
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-21 17:16:41 +0200

    210123

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './css/styles.css';
+import { fetchWeather } from './js/weather-api';
+import { showWeatherMarkup } from './js/showWeatherCard';
+// import displayWeather from './js/showWeatherCars';
+import refs from './js/refs';
+import { Notify } from 'notiflix';
+
+refs.button.addEventListener('click', search);
+refs.searchBar.addEventListener('keyup', enterSearch);
+
+// fetchWeather('Kyiv').then(response => showWeatherMarkup(response));
+
+function search() {
+  let searchCountry = refs.searchBar.value.trim();
+
+  fetchWeather(searchCountry)
+    .then(response => {
+      console.log(response);
+
+      const markup = showWeatherMarkup(response);
+
+      refs.weather.innerHTML = markup;
+      Notify.info(
+        `Everything is fine, we found the weather in ${searchCountry}`
+      );
+    })
+    .catch(error => {
+      // Notify.failure('Error, no weather found for that name');
+      console.log(error);
+    });
+}
+
+function enterSearch(event) {
+  if (event.key === 'Enter') {
+    search();
+  }
+}
---
+++ src/js/refs.js
@@ -0,0 +1,11 @@
+export default {
+  city: document.querySelector('.city'),
+  icon: document.querySelector('.icon'),
+  description: document.querySelector('.description'),
+  temp: document.querySelector('.temp'),
+  humidity: document.querySelector('.humidity'),
+  wind: document.querySelector('.wind'),
+  weather: document.querySelector('.weather'),
+  searchBar: document.querySelector('.search-bar'),
+  button: document.querySelector('.search button'),
+};
