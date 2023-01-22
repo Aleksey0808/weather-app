eb43f893a38db71d023a6e21bb92db1bfcde4aaf
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-22 22:22:34 +0200

    212223221212

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-import './css/styles.css';
-import { fetchWeather } from './js/weather-api';
-import { showWeatherMarkup } from './js/showWeatherCard';
-// import displayWeather from './js/showWeatherCars';
-import refs from './js/refs';
-import { Notify } from 'notiflix';
+// import './css/styles.css';
+// import { fetchWeather } from './js/weather-api';
+// import { showWeatherMarkup } from './js/showWeatherCard';
+// // import displayWeather from './js/showWeatherCars';
+// import refs from './js/refs';
+// import { Notify } from 'notiflix';
 
-const BASE_URL = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`;
-fetch(BASE_URL)
-  .then(response => {
-    return response.json();
-  })
-  .then(response => console.log(response));
+// const BASE_URL = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`;
+// fetch(BASE_URL)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(response => console.log(response));
 
-refs.button.addEventListener('click', search);
-refs.searchBar.addEventListener('keyup', enterSearch);
+// refs.button.addEventListener('click', search);
+// refs.searchBar.addEventListener('keyup', enterSearch);
 
-// fetchWeather('Kyiv').then(response => showWeatherMarkup(response));
+// // fetchWeather('Kyiv').then(response => showWeatherMarkup(response));
 
-function search() {
-  let searchCountry = refs.searchBar.value.trim();
+// function search() {
+//   let searchCountry = refs.searchBar.value.trim();
 
-  fetchWeather(searchCountry)
-    .then(response => {
-      console.log(response);
-      // console.log(showWeatherMarkup(response));
-      const markup = showWeatherMarkup(response);
-      const arrayAoi = Array.from(markup);
-      console.log(arrayAoi);
-      if (response.ok) {
-        const markup = response.map(response => showWeatherMarkup(response));
-        refs.weather.innerHTML = markup.join('');
-      }
-      Notify.info(
-        `Everything is fine, we found the weather in ${searchCountry}`
-      );
-    })
-    .catch(error => {
-      // Notify.failure('Error, no weather found for that name');
-      console.log(error);
-    });
-}
+//   fetchWeather(searchCountry)
+//     .then(response => {
+//       console.log(response);
+//       // console.log(showWeatherMarkup(response));
+//       const markup = showWeatherMarkup(response);
+//       const arrayAoi = Array.from(markup);
+//       console.log(arrayAoi);
+//       if (response.ok) {
+//         const markup = response.map(response => showWeatherMarkup(response));
+//         refs.weather.innerHTML = markup.join('');
+//       }
+//       Notify.info(
+//         `Everything is fine, we found the weather in ${searchCountry}`
+//       );
+//     })
+//     .catch(error => {
+//       // Notify.failure('Error, no weather found for that name');
+//       console.log(error);
+//     });
+// }
 
-function enterSearch(event) {
-  if (event.key === 'Enter') {
-    search();
-  }
-}
+// function enterSearch(event) {
+//   if (event.key === 'Enter') {
+//     search();
+//   }
+// }
