b807b3bd9ab421366b5cba30412244d6a0d913b2
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-22 22:26:11 +0200

    ирвартваотвпрворваорварварва

--- src/index.js
+++ src/index.js
@@ -17,29 +17,29 @@ refs.searchBar.addEventListener('keyup', enterSearch);
 
 // fetchWeather('Kyiv').then(response => showWeatherMarkup(response));
 
-function search() {
-  let searchCountry = refs.searchBar.value.trim();
+// function search() {
+//   let searchCountry = refs.searchBar.value.trim();
 
-  fetchWeather(searchCountry)
-    .then(response => {
-      console.log(response);
-      // console.log(showWeatherMarkup(response));
-      const markup = showWeatherMarkup(response);
-      const arrayAoi = Array.from(markup);
-      console.log(arrayAoi);
-      if (response.ok) {
-        const markup = response.map(response => showWeatherMarkup(response));
-        refs.weather.innerHTML = markup.join('');
-      }
-      Notify.info(
-        `Everything is fine, we found the weather in ${searchCountry}`
-      );
-    })
-    .catch(error => {
-      // Notify.failure('Error, no weather found for that name');
-      console.log(error);
-    });
-}
+// fetchWeather(searchCountry)
+// .then(response => {
+// console.log(response);
+// console.log(showWeatherMarkup(response));
+// const markup = showWeatherMarkup(response);
+// const arrayAoi = Array.from(markup);
+// console.log(arrayAoi);
+// if (response.ok) {
+//   const markup = response.map(response => showWeatherMarkup(response));
+//   refs.weather.innerHTML = markup.join('');
+// }
+//   Notify.info(
+//     `Everything is fine, we found the weather in ${searchCountry}`
+//   );
+// })
+//     .catch(error => {
+//       // Notify.failure('Error, no weather found for that name');
+//       console.log(error);
+//     });
+// }
 
 function enterSearch(event) {
   if (event.key === 'Enter') {
