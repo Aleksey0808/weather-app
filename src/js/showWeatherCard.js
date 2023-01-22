8b1f772cb5769c16816fa6e7dc2da808fb6a3458
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-22 22:24:23 +0200

    рварывар

--- src/js/showWeatherCard.js
+++ src/js/showWeatherCard.js
@@ -1,13 +1,13 @@
-// // export function showWeatherMarkup(value) {
-// //   return `<h2 class="city">Weather in ${data.name}</h2>
-// //   <h1 class="temp">${data.main.temp}°C</h1>
-// //   <div class="flex">
-// //     <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="" class="icon" />
-// //     <div class="description">${data.weather[0].description}</div>
-// //   </div>
-// //   <div class="humidity">Humidity: ${data.main.humidity}%</div>
-// //   <div class="wind">Wind speed: ${data.wind.speed} km/h</div>`;
-// // }
+export function showWeatherMarkup(value) {
+  return `<h2 class="city">Weather in ${data.name}</h2>
+  <h1 class="temp">${data.main.temp}°C</h1>
+  <div class="flex">
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="" class="icon" />
+    <div class="description">${data.weather[0].description}</div>
+  </div>
+  <div class="humidity">Humidity: ${data.main.humidity}%</div>
+  <div class="wind">Wind speed: ${data.wind.speed} km/h</div>`;
+}
 // export function showWeatherMarkup(data) {
 //   console.log(data.main.temp);
 //   console.log('hello');
@@ -21,22 +21,22 @@
 //    <div class="wind">Wind speed: ${data.wind.speed} km/h</div>`;
 // }
 
-// // import refs from '../js/refs';
+// import refs from '../js/refs';
 
-// // export default function displayWeather(data) {
-// //   const { name } = data;
-// //   const { icon, description } = data.weather[0];
-// //   const { temp, humidity } = data.main;
-// //   const { speed } = data.wind;
+// export default function displayWeather(data) {
+//   const { name } = data;
+//   const { icon, description } = data.weather[0];
+//   const { temp, humidity } = data.main;
+//   const { speed } = data.wind;
 
-// //   refs.city.textContent = `Weather in ${name}`;
-// //   refs.icon.src = `https://openweathermap.org/img/wn/${icon}.png`;
-// //   refs.description.textContent = description;
-// //   refs.temp.textContent = `${temp}°C`;
-// //   refs.humidity.textContent = `Humidity: ${humidity}%`;
-// //   refs.wind.textContent = `Wind speed: ${speed}km/h`;
-// //   refs.weather.classList.remove('loading');
+//   refs.city.textContent = `Weather in ${name}`;
+//   refs.icon.src = `https://openweathermap.org/img/wn/${icon}.png`;
+//   refs.description.textContent = description;
+//   refs.temp.textContent = `${temp}°C`;
+//   refs.humidity.textContent = `Humidity: ${humidity}%`;
+//   refs.wind.textContent = `Wind speed: ${speed}km/h`;
+//   refs.weather.classList.remove('loading');
 
-// //   document.body.style.backgroundImage =
-// //     "url('https://source.unsplash.com/1600x900/?" + name + "')";
-// // }
+//   document.body.style.backgroundImage =
+//     "url('https://source.unsplash.com/1600x900/?" + name + "')";
+// }
--- src/js/weather-api.js
+++ src/js/weather-api.js
@@ -1,15 +1,15 @@
-// import { Notify } from 'notiflix';
+import { Notify } from 'notiflix';
 
-// export function fetchWeather(city) {
-//   const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export function fetchWeather(city) {
+  const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-//   const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
-//   return fetch(`${BASE_URL}?q=${city}&units=metric&appid=${apiKey}`).then(
-//     response => {
-//       if (!response.ok) {
-//         throw new Error(response.statusText);
-//       }
-//       return response.json();
-//     }
-//   );
-// }
+  const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
+  return fetch(`${BASE_URL}?q=${city}&units=metric&appid=${apiKey}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    }
+  );
+}
