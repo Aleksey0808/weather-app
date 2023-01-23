fd89209af184fc548f0d29c7912164cf685cdf37
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-23 23:23:30 +0200

    2301234

--- src/index.js
+++ src/index.js
@@ -5,28 +5,24 @@ import { showWeatherMarkup } from './js/showWeatherCard';
 import refs from './js/refs';
 import { Notify } from 'notiflix';
 
+const URL_IMAGE = 'https://source.unsplash.com/1600x900/';
+
 refs.button.addEventListener('click', search);
 refs.searchBar.addEventListener('keyup', enterSearch);
 
-// fetchWeather('Kyiv').then(response => showWeatherMarkup(response));
-
-function search() {
+async function search() {
   let searchCountry = refs.searchBar.value.trim();
 
-fetchWeather(searchCountry)
-.then(response => {
-console.log(response);
-console.log(showWeatherMarkup(response));
-console.log(refs.weather)
-
-
-  const markup = showWeatherMarkup(response)
-  refs.weather.innerHTML = markup;
-  Notify.info(
-    `Everything is fine, we found the weather in ${searchCountry}`
-  );
-
-})
+  await fetchWeather(searchCountry)
+    .then(response => {
+      console.log(response);
+      document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?"${response.name}"')`;
+      const markup = showWeatherMarkup(response);
+      refs.weather.innerHTML = markup;
+      Notify.info(
+        `Everything is fine, we found the weather in ${searchCountry}`
+      );
+    })
     .catch(error => {
       Notify.failure('Error, no weather found for that name');
       console.log(error);
--- src/js/weather-api.js
+++ src/js/weather-api.js
@@ -1,15 +1,34 @@
 import { Notify } from 'notiflix';
+import axios from 'axios';
 
-export function fetchWeather(city) {
-  const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+// export async function fetchWeather(city) {
+//   const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-  const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
-  return fetch(`${BASE_URL}?q=${city}&units=metric&appid=${apiKey}`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    }
-  );
+//   const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
+//   return await fetch(`${BASE_URL}?q=${city}&units=metric&appid=${apiKey}`).then(
+//     response => {
+//       if (!response.ok) {
+//         throw new Error(response.statusText);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
+export async function fetchWeather(city) {
+  try {
+    return await axios
+      .get(BASE_URL, {
+        params: {
+          appid: apiKey,
+          q: city,
+          units: 'metric',
+        },
+      })
+      .then(response => response.data);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
