e1843bfcee8b59b3a887cac4a3992ba9294eeebd
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-01-23 14:56:08 +0200

    bug/fix

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ import { showWeatherMarkup } from './js/showWeatherCard';
 import refs from './js/refs';
 import { Notify } from 'notiflix';
 
-// const BASE_URL = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`;
-// fetch(BASE_URL)
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(response => console.log(response));
-
 refs.button.addEventListener('click', search);
 refs.searchBar.addEventListener('keyup', enterSearch);
 
